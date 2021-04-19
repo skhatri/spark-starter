425be26c0551ad91059c4a669c3d868c0f6e7767
Suresh Khatri <servlet2@msn.com>
2021-04-19 12:27:22 +1000

    add sql functions and DataFrame imports

--- src/main/scala/com/github/skhatri/spark/SparkEPLStandings.scala
+++ src/main/scala/com/github/skhatri/spark/SparkEPLStandings.scala
@@ -1,5 +1,7 @@
 package com.github.skhatri.spark
 
+import org.apache.spark.sql.DataFrame
+
 case class HomeAwayScore(home: Int, away: Int, home_points: Int, away_points: Int) extends Serializable {
 }
 
@@ -15,6 +17,7 @@ object SparkEPLStandings extends App {
     .getOrCreate()
 
   import spark.implicits._
+  import org.apache.spark.sql.functions._
 
   //Round,Date,Team 1,FT,Team 2
   val files = Seq("epl2012-part1.csv", "epl2012-part2.csv", "epl2012-part3.csv", "epl2012-part4.csv")
@@ -28,31 +31,37 @@ object SparkEPLStandings extends App {
     HomeAwayScore(parts(0).toInt, parts(1).toInt, home_points, away_points)
   })
 
-  val df = spark.read.option("header", true)
-    .csv(files: _*)
-    .selectExpr("week", "match_date", "home", "score", "away")
-    .selectExpr("*", "home_away_score(score) as match_score")
 
-  df.show(5, false)
+  def loadData(): DataFrame = {
+    val df: DataFrame = spark.read.option("header", true)
+      .csv(files: _*)
+      .selectExpr("week", "match_date", "home", "score", "away")
+      .selectExpr("*", "home_away_score(score) as match_score")
 
-  def findTeamsInPremierLeague(): Unit = {
+    df.show(5, false)
+    df
   }
 
-  def findAllMatchesByTeam(name: String): Unit = {
+  def findTeamsInPremierLeague(df: DataFrame): Unit = {
+
   }
 
-  def findBiggestWinByAnyTeam(): Unit = {
+  def findAllMatchesByTeam(df: DataFrame, name: String): Unit = {
+
   }
 
-  def findTeamsPoints(name: String): Int = {
+  def findBiggestWinByAnyTeam(df: DataFrame): Unit = {
+  }
+
+  def findTeamsPoints(df: DataFrame, name: String): Int = {
     0
   }
 
-  findTeamsInPremierLeague()
-  findAllMatchesByTeam("Manchester United FC")
-  findTeamsPoints("Arsenal FC")
-
-  findBiggestWinByAnyTeam()
+  val df: DataFrame = loadData()
+  findTeamsInPremierLeague(df)
+  findAllMatchesByTeam(df, "Manchester United FC")
+  findTeamsPoints(df, "Arsenal FC")
+  findBiggestWinByAnyTeam(df)
 
 
 }
